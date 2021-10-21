f729eccdcf9e1cbdb3b0d979013c71f0d4dafff8
Gabriel Mendes <gmendescamargodebarros@gmail.com>
2021-10-21 16:00:53 -0300

    second commit

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -6,6 +6,17 @@ export function MessageList() {
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
 
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>não vejo a hora de começar esse dowhile</p>
+          <div className={styles.messageUser}>
+            <div className={styles.UserImage}>
+              <img src="https://github.com/gabriel-mendes-camargo-de-barros.png" alt="Gabriel Mendes" />
+            </div>
+            <span>Gabrie Mendes</span>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
