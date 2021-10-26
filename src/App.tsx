dc10295a0beadd0b1fcdafaf31c8bb7e731908af
Gabriel Mendes <gmendescamargodebarros@gmail.com>
2021-10-26 17:43:11 -0300

    corrigindo layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { AuthContext } from './contexts/auth';
 // pages
 import { LoginBox } from './components/LoginBox';
 import { MessageList } from './components/MessageList';
+import { SendMessageForm } from './components/SendMessageForm';
 
 // styles
 import appStyles from './styles/App.module.scss';
-import { SendMessageForm } from './components/SendMessageForm';
 
 function App() {
   const { user } = useContext(AuthContext);
